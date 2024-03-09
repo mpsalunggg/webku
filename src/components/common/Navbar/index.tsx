56b0330cac0b0e999c8990369d7b5cfdb72fe101
mpsalunggg <putrasatria893@gmail.com>
2024-03-09 18:03:30 +0800

    feat: setup shadcn, create navbar, etc

---
+++ src/components/common/Navbar/index.tsx
@@ -0,0 +1,28 @@
+import { Menu } from '@/constants/menu';
+import { FC } from 'react';
+import { ModeToggle } from '../ModeToggle';
+
+const Navbar: FC = () => {
+  return (
+    <header className="h-24 flex items-center">
+      <div className="container flex items-center justify-between">
+        <div className="flex items-end">
+          <p className="font-extrabold text-3xl text-blue-400">Web</p>
+          <p className="font-semibold text-md italic">Ku</p>
+        </div>
+        <div className="flex items-center gap-8 italic">
+          {Menu.map(item => (
+            <p
+              key={item.id}
+              className="hover:text-blue-400 cursor-pointer hover:border-b hover:border-blue-400 duration-100"
+            >
+              {item.name}
+            </p>
+          ))}
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Navbar;
---
+++ src/constants/menu.ts
@@ -0,0 +1,16 @@
+import { MenuType } from '@/types/menu';
+
+export const Menu: MenuType[] = [
+  {
+    id: 1,
+    name: 'Project'
+  },
+  {
+    id: 2,
+    name: 'Experience'
+  },
+  {
+    id: 3,
+    name: 'Skill'
+  }
+];
