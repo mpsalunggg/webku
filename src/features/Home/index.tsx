49dbb29866159aaf23491c8eb5ec01a2b53c7d09
mpsalunggg <putrasatria893@gmail.com>
2024-04-14 22:37:07 +0800

    chore: remove justify center on main page

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -2,7 +2,7 @@ import { Typewriter } from 'nextjs-simple-typewriter';
 
 const HomePage = () => {
   return (
-    <main className="container h-full flex md:flex-row flex-col-reverse md:items-center justify-center md:gap-0 gap-8 mb-12">
+    <main className="container h-full flex md:flex-row flex-col-reverse md:items-center md:gap-0 gap-8 mb-12">
       <div className="flex flex-col gap-3">
         <h1 className="font-bold md:text-5xl text-4xl text-blue-400 drop-shadow-lg">
           Hello i&apos;m Putra👋
@@ -29,9 +29,6 @@ const HomePage = () => {
           </span>
         </p>
       </div>
-      {/* <Avatar className="md:w-96 md:h-96 w-52 h-52 border-4 dark:border-white border-blue-400 shadow-lg">
-          <AvatarImage src="/profile.svg" />
-        </Avatar> */}
     </main>
   );
 };
