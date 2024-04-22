014b9075636913e6cdb6e16acb261abdf67b2da1
mpsalunggg <putrasatria893@gmail.com>
2024-04-22 22:24:05 +0800

    feat: add animation on stack page

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -1,11 +1,14 @@
 import { NextPage } from 'next';
 import Main from '@/components/common/Main';
 import StackPage from '@/features/Stack';
+import Template from '@/components/common/Template';
 
 const Stack: NextPage = () => {
   return (
     <Main className="justify-between">
-      <StackPage />
+      <Template key="stack">
+        <StackPage />
+      </Template>
     </Main>
   );
 };
