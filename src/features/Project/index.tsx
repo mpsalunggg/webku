0077bb89f73e91fe4d16d4751fe0aa44f1d0d9db
mpsalunggg <putrasatria893@gmail.com>
2024-04-28 20:38:55 +0800

    chore: width title section project

--- src/features/Project/index.tsx
+++ src/features/Project/index.tsx
@@ -3,12 +3,15 @@ import { OurProjects } from '@/constants/project';
 
 const ProjectPage = () => {
   return (
-    <section className="container">
-      <div className="my-8 text-center space-y-2">
+    <section className="container flex flex-col items-center">
+      <div className="my-8 text-center space-y-2 w-2/3">
         <h1 className="text-2xl font-semibold italic">Our Projects</h1>
         <p>
           Here are some of the results of my learning about programming,
-          hopefully it can inspire others. You can check my <span>github</span>
+          hopefully it can inspire others. You can check my{' '}
+          <span className="underline italic text-blue-400 cursor-pointer">
+            github
+          </span>
         </p>
       </div>
       <div className="flex justify-center flex-col gap-6 mb-8">
@@ -18,7 +21,7 @@ const ProjectPage = () => {
             className="relative w-full lg:flex lg:min-h-92 rounded-xl transition-all hover:shadow-lg cursor-pointer"
           >
             <div
-              className="rounded-t-lg lg:min-w-96 w-full bg-white bg-cover bg-top shadow-lg h-48 rounded-lg"
+              className="rounded-t-lg lg:min-w-96 w-full bg-white bg-cover bg-top shadow-lg lg:h-48 h-40 rounded-lg"
               style={{
                 backgroundImage: `url('${item.image}')`
               }}
