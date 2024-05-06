21f0afcdd23d84dad628391e23d8a905d8333a3e
mpsalunggg <putrasatria893@gmail.com>
2024-05-06 19:41:49 +0800

    add: header experiences

--- src/features/Experience/index.tsx
+++ src/features/Experience/index.tsx
@@ -1,4 +1,16 @@
 const ExperiencePage = () => {
-  return <section className="container"></section>;
+  return (
+    <section className="container flex flex-col items-center w-full">
+      <div className="my-8 text-center space-y-2 w-full">
+        <h1 className="text-2xl font-semibold italic">
+          Experiences & Activities
+        </h1>
+        <p className="">
+          Here are some of the interesting experiences and activities that I
+          have participated in
+        </p>
+      </div>
+    </section>
+  );
 };
 export default ExperiencePage;
