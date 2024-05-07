6967eab7ef6ee8daa4dacd892ebd668d6331176c
mpsalunggg <putrasatria893@gmail.com>
2024-05-07 21:34:25 +0800

    chore: change key

--- src/components/display/CardStack/index.tsx
+++ src/components/display/CardStack/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
-import { CardStackProps } from '@/types';
 import { Icon } from '@iconify/react';
+import { motion } from 'framer-motion';
+import { CardStackProps } from '@/types';
 import {
   Tooltip,
   TooltipContent,
@@ -14,14 +15,26 @@ const CardStack: FC<CardStackProps> = ({ title, data }) => {
       <h1 className="text-3xl font-semibold italic">{title}</h1>
       <div className="flex gap-2 justify-center">
         {data.map(item => (
-          <TooltipProvider key={item.id}>
-            <Tooltip>
-              <TooltipTrigger>
-                <Icon key={item.id} icon={item.icon} fontSize={'30'} />
-              </TooltipTrigger>
-              <TooltipContent>{item.title}</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+          <motion.div
+            key={item.id}
+            className="box"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.4,
+              delay: item.id * 0.1,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+          >
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Icon key={item.id} icon={item.icon} fontSize={'30'} />
+                </TooltipTrigger>
+                <TooltipContent>{item.title}</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </motion.div>
         ))}
       </div>
     </div>
--- src/features/Experience/index.tsx
+++ src/features/Experience/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { motion } from 'framer-motion';
 import { Experiences } from '@/constants/experience';
 
 const ExperiencePage = () => {
@@ -16,27 +18,39 @@ const ExperiencePage = () => {
         {Experiences.reverse().map(item => {
           const { id, role, place, duration, description, company } = item;
           return (
-            <div key={id}>
+            <motion.div
+              key={id}
+              className="box"
+              initial={{ opacity: 0, scale: 0.3 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.4,
+                delay: id * 0.3,
+                ease: [0, 0.71, 0.6, 1.01]
+              }}
+            >
               <div>
-                <div className="flex justify-between items-center">
-                  <h4 className="scroll-m-20 lg:text-xl text-md font-semibold tracking-tight text-blue-400">
-                    {role}
-                  </h4>
-                  <p className="font-semibold tracking-tight text-blue-400">
-                    {duration}
-                  </p>
+                <div>
+                  <div className="flex justify-between items-center">
+                    <h4 className="scroll-m-20 lg:text-xl text-md font-semibold tracking-tight text-blue-400">
+                      {role}
+                    </h4>
+                    <p className="font-semibold tracking-tight text-blue-400">
+                      {duration}
+                    </p>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <p className="italic font-semibold">{company}</p>
+                    <p className="italic font-semibold">{place}</p>
+                  </div>
                 </div>
-                <div className="flex justify-between items-center">
-                  <p className="italic font-semibold">{company}</p>
-                  <p className="italic font-semibold">{place}</p>
-                </div>
+                <ul className="mb-6 ml-6 list-disc [&>li]:mt-2">
+                  {description.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                  ))}
+                </ul>
               </div>
-              <ul className="mb-6 ml-6 list-disc [&>li]:mt-2">
-                {description.map(item => (
-                  <li key={item.id}>{item.text}</li>
-                ))}
-              </ul>
-            </div>
+            </motion.div>
           );
         })}
       </div>
