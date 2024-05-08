cee6a0b0eb988a018ec763ddb2f8c32753a4fd3b
mpsalunggg <putrasatria893@gmail.com>
2024-05-09 00:11:04 +0800

    fix: change text to span

--- src/features/Experience/index.tsx
+++ src/features/Experience/index.tsx
@@ -20,7 +20,6 @@ const ExperiencePage = () => {
           return (
             <motion.div
               key={id}
-              className="box"
               initial={{ opacity: 0, scale: 0.3 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
@@ -29,19 +28,17 @@ const ExperiencePage = () => {
                 ease: [0, 0.71, 0.6, 1.01]
               }}
             >
-              <div>
-                <div className="flex justify-between items-center">
-                  <h1 className="scroll-m-20 lg:text-xl text-sm font-semibold tracking-tight text-blue-400">
-                    {role}
-                  </h1>
-                  <p className="font-semibold lg:text-lg text-sm tracking-tight text-blue-400">
-                    {duration}
-                  </p>
-                </div>
-                <div className="flex justify-between items-center lg:text-md text-sm">
-                  <p className="italic font-semibold">{company}</p>
-                  <p className="italic font-semibold">{place}</p>
-                </div>
+              <div className="flex justify-between items-center">
+                <span className="scroll-m-20 lg:text-xl text-sm font-semibold tracking-tight text-blue-400">
+                  {role}
+                </span>
+                <span className="font-semibold lg:text-lg text-sm tracking-tight text-blue-400">
+                  {duration}
+                </span>
+              </div>
+              <div className="flex justify-between items-center lg:text-md text-sm">
+                <p className="italic font-semibold">{company}</p>
+                <p className="italic font-semibold">{place}</p>
               </div>
               <ul className="mb-6 ml-6 list-disc [&>li]:mt-2 lg:text-md text-sm">
                 {description.map(item => (
