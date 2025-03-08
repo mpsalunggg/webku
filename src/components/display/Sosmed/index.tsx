20b9b8b72504d1e32f53dd05653f29a3ce44adab
mpsalunggg <putrasatria893@gmail.com>
2025-03-08 11:04:04 +0700

    adjust link github and linkedin

--- src/components/display/Sosmed/index.tsx
+++ src/components/display/Sosmed/index.tsx
@@ -8,13 +8,17 @@ const Sosmed = () => {
         className="rounded-full text-white bg-purple-600 hover:bg-purple-500 cursor-pointer"
         size="icon"
       >
-        <Github />
+        <a href="https://github.com/mpsalunggg">
+          <Github />
+        </a>
       </Button>
       <Button
         className="rounded-full text-white bg-blue-500 hover:bg-blue-400 cursor-pointer"
         size="icon"
       >
-        <Linkedin />
+        <a href="https://www.linkedin.com/in/muhamadputrasatria/">
+          <Linkedin />
+        </a>
       </Button>
     </div>
   );
