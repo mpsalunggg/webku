f142173eab3dde4af23c60d7aef33180baacc27f
mpsalunggg <putrasatria893@gmail.com>
2025-03-09 20:53:08 +0700

    create new section profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import { NextPage } from 'next';
 import HomePage from '@/features/Home';
 import Main from '@/components/common/Main';
 import Template from '../components/common/Template';
+import Profile from '@/components/display/About';
 
 const Home: NextPage = () => {
   return (
     <Main>
       <HomePage />
+      <Profile />
     </Main>
   );
 };
---
+++ src/components/display/About/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+const Profile = () => {
+  return (
+    <section id="profile" className="py-8">
+      <div>
+        <Image src="" width={200} height={200} alt="profile" />
+      </div>
+    </section>
+  );
+};
+export default Profile;
