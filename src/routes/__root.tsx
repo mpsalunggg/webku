287da6614a1a9839a7d49825ac9030737a75f5b5
mpsalunggg <putrasatria893@gmail.com>
2025-05-04 17:50:03 +0700

    chore: comment about link

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,9 +8,9 @@ export const Route = createRootRoute({
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
+        {/* <Link to="/about" className="[&.active]:font-bold">
           About
-        </Link>
+        </Link> */}
       </div>
       <hr />
       <Outlet />
