80cf78ec3d0549df346a5d8b53cfda8e8d2e6f57
mpsalunggg <putrasatria893@gmail.com>
2025-05-17 18:57:25 +0700

    feat: setup theme provider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import MainLayout from '@/components/layout/MainLayout'
+import { ThemeProvider } from '@/components/layout/ThemeProvider'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { Fragment } from 'react/jsx-runtime'
@@ -6,10 +7,12 @@ import { Fragment } from 'react/jsx-runtime'
 export const Route = createRootRoute({
   component: () => (
     <Fragment>
-      <MainLayout>
-        <Outlet />
-      </MainLayout>
-      <TanStackRouterDevtools />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <MainLayout>
+          <Outlet />
+        </MainLayout>
+        <TanStackRouterDevtools />
+      </ThemeProvider>
     </Fragment>
   ),
 })
