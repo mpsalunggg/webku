6eb32bcb349a3d3c9073547edc9a148bc4394afa
mpsalunggg <putrasatria893@gmail.com>
2025-06-15 01:46:31 +0700

    feat: add background dots yellow

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils'
 import React from 'react'
+import Background from './Background'
 
 const MainLayout = ({
   children,
@@ -11,11 +12,12 @@ const MainLayout = ({
   return (
     <div
       className={cn(
-        'transition-all min-h-screen px-4 flex flex-col lg:mx-96 md:mx-40 sm:mx-24 ',
+        'relative transition-all min-h-screen px-4 flex flex-col lg:mx-96 md:mx-40 sm:mx-24 ',
         className
       )}
     >
       {children}
+      <Background />
     </div>
   )
 }
