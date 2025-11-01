ffdde58a54c3a34d4ddd28f01b46ee5d40e25651
mpsalunggg <putrasatria893@gmail.com>
2025-11-01 19:30:57 +0700

    feat: migrate to tanstack start

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,55 @@
+// src/routes/__root.tsx
+/// <reference types="vite/client" />
+import type { ReactNode } from 'react'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from '@tanstack/react-router'
 import { ThemeProvider } from '@/components/layout/ThemeProvider'
 import MainLayout from '@/components/layout/MainLayout'
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { Fragment } from 'react/jsx-runtime'
 
 export const Route = createRootRoute({
-  component: () => (
-    <Fragment>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <MainLayout>
-          <Outlet />
-        </MainLayout>
-        <TanStackRouterDevtools />
-      </ThemeProvider>
-    </Fragment>
-  ),
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+  }),
+  component: RootComponent,
 })
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <MainLayout>{children}</MainLayout>
+          <TanStackRouterDevtools />
+        </ThemeProvider>
+        <Scripts />
+      </body>
+    </html>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import path from 'path'
+// import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
-    react(),
+    tsConfigPaths(),
+    tanstackStart(),
+    // react's vite plugin must come after start's vite plugin
+    viteReact(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, './src'),
+  //   },
+  // },
 })
